00571762e4bc3ac7eb691824cce056929cdc02e1
Septian A Tama <sfirst.tama@gmail.com>
2017-01-29 00:43:01 +0700

    add getfiles method

--- src/js/components/github-file-explorer/index.js
+++ src/js/components/github-file-explorer/index.js
@@ -1,1 +1,29 @@
 
+module.exports = {
+  template: require('./template.html'),
+  data: function() {
+    return {
+      path: '/',
+      files: []
+    };
+  },
+  props: {
+    user: {
+      type: String,
+      required: true
+    },
+    repo: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    getFiles: function() {
+      this.$http.get('https://api.github.com/repos/' + this.fullRepoUrl + '/contents' + this.path,
+      function(data) {
+        this.files = data;
+      })
+    }
+  }
+  created: if (this.username && this.repo) this.getFiles();
+}
