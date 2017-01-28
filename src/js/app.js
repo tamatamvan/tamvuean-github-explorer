d81572a50d91756521ec95f688a34048f71678a5
Septian A Tama <sfirst.tama@gmail.com>
2017-01-29 00:00:11 +0700

    add root element, data, and methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
-(function() {
-    console.log('Hello world');
-})();
+var Vue = require('vue');
+Vue.config.debug = true;
+
+new Vue({
+  el: '#container',
+  data: {
+    fullRepoName:'',
+    username: '',
+    repo: ''
+  },
+  methods: {
+    changeRepo: function(){
+      var splitData = this.fullRepoName.split('/');
+      this.username = splitData[0];
+      this.repo = splitData[1];
+
+      console.group("Vue Data");
+      console.log("Full Repo Name: ", this.fullRepoName);
+      console.log("Username: ", this.username);
+      console.log("Repo: ", this.repo);
+      console.groupEnd("Vue Data");
+    }
+  }
+})
